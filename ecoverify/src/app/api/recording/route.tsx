01878d6f7c1f31f0e16a39492864026bf88ab355
Moath-Ali <azsexc71@gmail.com>
2025-02-18 22:02:00 +0300

    Feature: Post request; add recording

--- ecoverify/src/app/api/recording/route.tsx
+++ ecoverify/src/app/api/recording/route.tsx
@@ -1,29 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import clientPromise from '../mongodb';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const client = await clientPromise;
-        const db = client.db('mydatabase'); // Replace with your DB name
-        const collection = db.collection('users'); // Replace with your collection
-
-        if (req.method === 'POST') {
-            const { name, email } = req.body;
-            if (!name || !email) {
-                return res.status(400).json({ error: 'Missing name or email' });
-            }
-
-            const result = await collection.insertOne({ name, email });
-            return res.status(201).json({ message: 'User added', data: result });
-        }
-
-        if (req.method === 'GET') {
-            const users = await collection.find({}).toArray();
-            return res.status(200).json({ users });
-        }
-
-        return res.status(405).json({ error: 'Method Not Allowed' });
-    } catch (error) {
-        return res.status(500).json({ error: 'Internal Server Error', details: error });
+export async function POST(req: Request, res: NextResponse) {
+  try {
+    const client = await clientPromise;
+    const db = client.db('EcoVerify'); 
+    const collection = db.collection('Recording'); 
+    
+    if (req.method === 'POST') {
+      
+      let { metadata, timestamp, sensor_data} = await req.json();
+      if (!metadata || !timestamp || !sensor_data) {
+        return NextResponse.json({ error: 'Missing metadata || !timestamp || sensor_data' });
+      }
+      timestamp = new Date(timestamp)
+      const result = await collection.insertOne({ metadata,  timestamp, sensor_data });
+      return NextResponse.json({ message: 'Recording added', data: result });
     }
+
+    return NextResponse.json({ error: 'Method Not Allowed' });
+  } catch (error) {
+    return NextResponse.json({ error: 'Internal Server Error', details: error });
+  }
 }
