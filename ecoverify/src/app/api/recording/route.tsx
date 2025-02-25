a27c4328e3bf80edfee5fa55109d7065363a62df
Moath-Ali <azsexc71@gmail.com>
2025-02-25 09:11:00 +0300

    Feature: handle many docs

--- ecoverify/src/app/api/recording/route.tsx
+++ ecoverify/src/app/api/recording/route.tsx
@@ -2,20 +2,52 @@ import { NextResponse } from 'next/server';
 import clientPromise from '../mongodb';
 
 export async function POST(req: Request, res: NextResponse) {
+
   try {
     const client = await clientPromise;
-    const db = client.db('EcoVerify'); 
-    const collection = db.collection('Recording'); 
-    
+    const db = client.db('EcoVerify');
+    const Recordingcollection = db.collection('Recording');
+    const Usercollection = db.collection('Users');
     if (req.method === 'POST') {
-      
-      let { metadata, timestamp, sensor_data} = await req.json();
-      if (!metadata || !timestamp || !sensor_data) {
-        return NextResponse.json({ error: 'Missing metadata || !timestamp || sensor_data' });
+      let data = await req.json();
+      const userData = await Usercollection.findOne({ "devices.deviceID": data['uuid'] })
+      let location = ""
+      if (userData)
+      for (var item of userData['devices']) {
+        if (item['deviceID'] === data['uuid']) {
+          location = item['location'];
+          break;
+        }
       }
-      timestamp = new Date(timestamp)
-      const result = await collection.insertOne({ metadata,  timestamp, sensor_data });
-      return NextResponse.json({ message: 'Recording added', data: result });
+
+      for (var item of data['data']) {
+
+        item['timestamp'] = new Date(item['timestamp']);
+
+
+      }
+      let docsToInsert = []
+      
+      if (userData)
+      for (var item of data['data']) {
+        docsToInsert.push({
+          'metadata': {
+            'company': userData['companyName'],
+            'location': location,
+            'uuid':data['uuid']
+          },
+          'sensor_data' :{
+            "temperature":item['temperature'],
+            "humidity":item['humidity']/100
+          },
+          'timestamp':item['timestamp']
+        })
+        
+      }
+      // console.log(docsToInsert)
+      
+      const result = await Recordingcollection.insertMany(docsToInsert);
+      return NextResponse.json({ message: 'Recording added', timestamp: new Date().toLocaleString(undefined, { timeZoneName: 'short' }) });
     }
 
     return NextResponse.json({ error: 'Method Not Allowed' });
@@ -23,3 +55,5 @@ export async function POST(req: Request, res: NextResponse) {
     return NextResponse.json({ error: 'Internal Server Error', details: error });
   }
 }
+
+
